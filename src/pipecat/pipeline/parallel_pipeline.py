8c3417c6c855b39da4ee750158c0ec6afdf165e1
manish <baghelmanish660@gmail.com>
2025-01-23 01:25:22 +0900

    copy parallel_pipeline from upstream latest

--- src/pipecat/pipeline/parallel_pipeline.py
+++ src/pipecat/pipeline/parallel_pipeline.py
@@ -5,37 +5,49 @@
 #
 
 import asyncio
-
 from itertools import chain
-from typing import List
-
-from pipecat.pipeline.base_pipeline import BasePipeline
-from pipecat.pipeline.pipeline import Pipeline
-from pipecat.processors.frame_processor import FrameDirection, FrameProcessor
-from pipecat.frames.frames import CancelFrame, EndFrame, Frame, StartFrame
+from typing import Awaitable, Callable, List
 
 from loguru import logger
 
+from pipecat.frames.frames import CancelFrame, EndFrame, Frame, StartFrame, SystemFrame
+from pipecat.pipeline.base_pipeline import BasePipeline
+from pipecat.pipeline.pipeline import Pipeline
+from pipecat.processors.frame_processor import FrameDirection, FrameProcessor
+
 
 class Source(FrameProcessor):
-    def __init__(self, upstream_queue: asyncio.Queue):
+    def __init__(
+        self,
+        upstream_queue: asyncio.Queue,
+        push_frame_func: Callable[[Frame, FrameDirection], Awaitable[None]],
+    ):
         super().__init__()
         self._up_queue = upstream_queue
+        self._push_frame_func = push_frame_func
 
     async def process_frame(self, frame: Frame, direction: FrameDirection):
         await super().process_frame(frame, direction)
 
         match direction:
             case FrameDirection.UPSTREAM:
-                await self._up_queue.put(frame)
+                if isinstance(frame, SystemFrame):
+                    await self._push_frame_func(frame, direction)
+                else:
+                    await self._up_queue.put(frame)
             case FrameDirection.DOWNSTREAM:
                 await self.push_frame(frame, direction)
 
 
 class Sink(FrameProcessor):
-    def __init__(self, downstream_queue: asyncio.Queue):
+    def __init__(
+        self,
+        downstream_queue: asyncio.Queue,
+        push_frame_func: Callable[[Frame, FrameDirection], Awaitable[None]],
+    ):
         super().__init__()
         self._down_queue = downstream_queue
+        self._push_frame_func = push_frame_func
 
     async def process_frame(self, frame: Frame, direction: FrameDirection):
         await super().process_frame(frame, direction)
@@ -44,7 +56,10 @@ class Sink(FrameProcessor):
             case FrameDirection.UPSTREAM:
                 await self.push_frame(frame, direction)
             case FrameDirection.DOWNSTREAM:
-                await self._down_queue.put(frame)
+                if isinstance(frame, SystemFrame):
+                    await self._push_frame_func(frame, direction)
+                else:
+                    await self._down_queue.put(frame)
 
 
 class ParallelPipeline(BasePipeline):
@@ -52,10 +67,11 @@ class ParallelPipeline(BasePipeline):
         super().__init__()
 
         if len(args) == 0:
-            raise Exception(f"ParallelPipeline needs at least one argument")
+            raise Exception("ParallelPipeline needs at least one argument")
 
         self._sources = []
         self._sinks = []
+        self._seen_ids = set()
 
         self._up_queue = asyncio.Queue()
         self._down_queue = asyncio.Queue()
@@ -70,8 +86,8 @@ class ParallelPipeline(BasePipeline):
                 raise TypeError(f"ParallelPipeline argument {processors} is not a list")
 
             # We will add a source before the pipeline and a sink after.
-            source = Source(self._up_queue)
-            sink = Sink(self._down_queue)
+            source = Source(self._up_queue, self._parallel_push_frame)
+            sink = Sink(self._down_queue, self._parallel_push_frame)
             self._sources.append(source)
             self._sinks.append(sink)
 
@@ -113,9 +129,6 @@ class ParallelPipeline(BasePipeline):
             await asyncio.gather(*[s.queue_frame(frame, direction) for s in self._sinks])
         elif direction == FrameDirection.DOWNSTREAM:
             # If we get a downstream frame we process it in each source.
-            # TODO(aleix): We are creating task for each frame. For real-time
-            # video/audio this might be too slow. We should use an already
-            # created task instead.
             await asyncio.gather(*[s.queue_frame(frame, direction) for s in self._sources])
 
         # If we get an EndFrame we stop our queue processing tasks and wait on
@@ -129,24 +142,25 @@ class ParallelPipeline(BasePipeline):
             if self._down_task:
                 await self._down_task
 
+    async def _parallel_push_frame(self, frame: Frame, direction: FrameDirection):
+        if frame.id not in self._seen_ids:
+            self._seen_ids.add(frame.id)
+            await self.push_frame(frame, direction)
+
     async def _process_up_queue(self):
         running = True
-        seen_ids = set()
         while running:
             frame = await self._up_queue.get()
-            if frame and frame.id not in seen_ids:
-                await self.push_frame(frame, FrameDirection.UPSTREAM)
-                seen_ids.add(frame.id)
+            if frame:
+                await self._parallel_push_frame(frame, FrameDirection.UPSTREAM)
             running = frame is not None
             self._up_queue.task_done()
 
     async def _process_down_queue(self):
         running = True
-        seen_ids = set()
         while running:
             frame = await self._down_queue.get()
-            if frame and frame.id not in seen_ids:
-                await self.push_frame(frame, FrameDirection.DOWNSTREAM)
-                seen_ids.add(frame.id)
+            if frame:
+                await self._parallel_push_frame(frame, FrameDirection.DOWNSTREAM)
             running = frame is not None
             self._down_queue.task_done()
